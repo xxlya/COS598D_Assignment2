80f83d45d1723679e771531c583db0e562ed7d0e
Xiaoxiao Li <xiaoxiao.li@yale.edu>
2021-02-25 12:33:51 -0500

    Update main_vanilla.py

--- CIFAR_10/main_vanilla.py
+++ CIFAR_10/main_vanilla.py
@@ -13,6 +13,9 @@ import torch.optim as optim
 from models import nin
 from torch.autograd import Variable
 
+import torchvision
+import torchvision.transforms as transforms
+
 def save_state(model, best_acc):
     print('==> Saving model ...')
     state = {
@@ -101,18 +104,32 @@ if __name__=='__main__':
     torch.cuda.manual_seed(1)
 
     # prepare the data
-    if not os.path.isfile(args.data+'/train_data'):
-        # check the data path
-        raise Exception\
-                ('Please assign the correct data path with --data <DATA_PATH>')
+    # if not os.path.isfile(args.data+'/train_data'):
+    #     # check the data path
+    #     raise Exception\
+    #             ('Please assign the correct data path with --data <DATA_PATH>')
 
-    trainset = data.dataset(root=args.data, train=True)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
-            shuffle=True, num_workers=2)
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
 
-    testset = data.dataset(root=args.data, train=False)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=100,
-            shuffle=False, num_workers=2)
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=128, shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=100, shuffle=False, num_workers=2)
 
     # define classes
     classes = ('plane', 'car', 'bird', 'cat',
